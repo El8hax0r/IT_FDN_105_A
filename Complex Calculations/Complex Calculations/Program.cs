c4a6c0def81eb0e9603d687a49a16fbde761ce8d
Matthew Mitchell <39917204+El8hax0r@users.noreply.github.com>
2019-10-17 18:44:36 -0700

    Add files via upload

---
+++ Complex Calculations/Complex Calculations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Complex_Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Seconds: ");
+            string sSeconds = Console.ReadLine();
+            int iSeconds = int.Parse(sSeconds);
+
+            int iDays = iSeconds / 86400; //seconds per day
+            iSeconds = iSeconds % 86400; //remainder
+
+            int iHours = iSeconds / 3600; //seconds per hour
+            iSeconds = iSeconds % 3600;
+
+            int iMinutes = iSeconds / 60; //seconds in one minute
+            iSeconds = iSeconds % 60;
+
+            Console.Write(sSeconds + "s = " + iDays.ToString() + " days, " + iHours.ToString() + " hours, " + iMinutes.ToString() + " minutes, and " + iSeconds.ToString() + " seconds");
+
+            Console.Write("\n\nPress any key to exit...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Find Smith/Find Smith/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Lastname
+{
+    static void Main()
+    {
+        Console.Write("Enter a person's full name:");
+        string str = Console.ReadLine();
+
+        if (str.Contains("Smith"))
+        {
+            Console.WriteLine("You found a Smith!");
+        }
+        else
+        {
+            Console.WriteLine("No Smith was found!");
+        }
+
+        Console.ReadLine();
+    }
+}
